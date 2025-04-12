f17ddc86d7c1a7574d95cb38c481204a788b9c5f
shivampawar777 <pawarshivam826@gmail.com>
2025-04-13 03:44:44 +0530

    Add files via upload

--- employee_management_app/urls.py
+++ employee_management_app/urls.py
@@ -9,6 +9,7 @@ from .forms import LoginForm
 urlpatterns = [
     path('', start),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html', authentication_form=LoginForm)),
+    path('change-password/', change_password),
     path('logout/', logout_view),
     path('home/',home),
     path('add-emp/',add_emp),
