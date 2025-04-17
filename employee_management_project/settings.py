ed7f6c66813b3902cd039a50e8bce59e71de348b
Shivam Pawar <147264094+shivampawar183@users.noreply.github.com>
2025-04-17 18:22:39 +0530

    Update settings.py

--- employee_management_project/settings.py
+++ employee_management_project/settings.py
@@ -24,6 +24,7 @@ ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split()
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
