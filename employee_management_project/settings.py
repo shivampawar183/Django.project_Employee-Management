58eaa27938fbeda57e369e2439b8bd4f669add74
Shivam Pawar <147264094+shivampawar183@users.noreply.github.com>
2025-04-17 19:02:18 +0530

    Update settings.py

--- employee_management_project/settings.py
+++ employee_management_project/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
